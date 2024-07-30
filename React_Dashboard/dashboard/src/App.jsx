2273c95f85a91fe9d86ae523a6ef32955dc947b0
Dineshreddy28 <pelleti.d@northeastern.edu>
2024-07-30 18:21:24 -0400

    changes

---
+++ React_Dashboard/dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPage from './VendorDashboard/Components/pages/LandingPage'
+import './App.css'
+const App = () => {
+  return (
+    <center><div>
+    <LandingPage/>
+  </div></center>
+  )
+}
+
+export default App
---
+++ React_Dashboard/dashboard/src/VendorDashboard/Components/pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../NavBar'
+
+const LandingPage = () => {
+  return (
+    <center><div>
+    <> <div className="landingpage">
+      <NavBar/>
+      </div> 
+    </>
+  </div></center>
+    
+  )
+}
+
+export default LandingPage
